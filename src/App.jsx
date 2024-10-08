3c971f29a5ed861e2fc2d29c49d6feb2890c0da3
phaniraj35 <phaniitng@gmail.com>
2024-10-08 18:12:41 +0530

    Initial setup & desktop nav implemented

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <main className="text-sm text-neutral-300 antialiased">
+      <Navbar />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+import logo from "../assets/logo.png";
+import { RiCloseFill, RiMenu3Line } from "@remixicon/react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsOpen((prev) => !prev);
+  }
+
+  const ariaLabel = isOpen ? "Close Menu" : "Open Menu";
+  const hamburgerIcon = isOpen ? <RiCloseFill /> : <RiMenu3Line />;
+
+  return (
+    <nav className="fixed top-0 right-0 left-0 z-50 mt-2">
+      <div className="text-neutral-500 bg-black/60 backdrop-blur-md max-w-7xl mx-auto px-4 py-3 flex justify-between items-center rounded-xl border border-neutral-800">
+        {/* logo image */}
+        <img src={logo} alt="Logo image" width={120} height={24} />
+
+        {/* hidden on mobile */}
+        <div className="hidden md:flex space-x-6">
+          <CenterNavMenu />
+        </div>
+
+        {/* hidden on mobile */}
+        <div className="hidden md:flex items-center space-x-4">
+          <NavLink href="#">Login</NavLink>
+
+          <NavActionMenu />
+        </div>
+
+        {/* hamburger for mobile */}
+        <div className="md:hidden">
+          <button
+            onClick={toggleMenu}
+            className="text-white"
+            aria-label={ariaLabel}
+          >
+            {hamburgerIcon}
+          </button>
+        </div>
+      </div>
+
+      {isOpen && (
+        <div className="md:hidden bg-neutral-900/60 backdrop-blur-md border border-neutral-800 p-4 rounded-xl mt-2">
+          <div className="flex flex-col space-y-4">
+            <CenterNavMenu />
+            <NavLink href="#">Login</NavLink>
+
+            <div className="flex justify-evenly">
+              <NavActionMenu />
+            </div>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
+
+function NavLink({ href, children, ...props }) {
+  return (
+    <a href={href} className="hover:text-neutral-200" {...props}>
+      {children}
+    </a>
+  );
+}
+
+function NavActionButton({ href, className, children, ...props }) {
+  return (
+    <a
+      href={href}
+      className={`text-white py-2 px-4 rounded-lg transition ${className}`}
+      {...props}
+    >
+      {children}
+    </a>
+  );
+}
+
+function CenterNavMenu() {
+  return (
+    <>
+      <NavLink href="#works">How it works</NavLink>
+      <NavLink href="#pricing">Pricing</NavLink>
+      <NavLink href="#testimonials">Testimonials</NavLink>
+    </>
+  );
+}
+
+function NavActionMenu() {
+  return (
+    <>
+      <NavActionButton
+        href="#"
+        className="border border-neutral-700 hover:bg-neutral-700"
+      >
+        Get a Demo
+      </NavActionButton>
+
+      <NavActionButton href="#" className="bg-blue-600 hover:bg-blue-500">
+        Start Free Trial
+      </NavActionButton>
+    </>
+  );
+}
