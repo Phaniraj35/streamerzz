722a85eaeae2c91ba6508649a3ed4c60ef3ba5b9
phaniraj35 <phaniitng@gmail.com>
2024-10-09 23:01:41 +0530

    fixed typo of styles in Header.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
       variants={containerVariants}
       initial="hidden"
       animate="visible"
-      className="pt-28 lg:pg-36"
+      className="pt-28 lg:pt-36"
     >
       <div className="max-w-7xl mx-auto px-4 flex flex-col items-center">
         <motion.div
