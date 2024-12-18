76ba9b47cf447991bde422d35ae4dd1e185e0208
phaniraj35 <phaniitng@gmail.com>
2024-12-18 17:54:10 +0530

    section id for key feautures

--- src/components/KeyFeatures.jsx
+++ src/components/KeyFeatures.jsx
@@ -25,7 +25,7 @@ const featureVariants = {
 
 const KeyFeatures = () => {
   return (
-    <section>
+    <section id="features">
       <div className="max-w-7xl mx-auto px-4 mt-20">
         <div className="text-center mb-12 border-t border-neutral-800">
           <h2 className="text-3xl md:text-5xl mt-20 tracking-tighter bg-gradient-to-t from-neutral-50 via-neutral-300 to-neutral-600 bg-clip-text text-transparent">
